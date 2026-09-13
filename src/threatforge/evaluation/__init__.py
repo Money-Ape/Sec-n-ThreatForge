e9265a09d7d2278450347c0c5a79714fa387db3d
Money-Ape <work.moneyapenetwork@gmail.com>
2026-09-13 23:51:50 +0530

    feat: add evaluation package

---
+++ src/threatforge/evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""
+This script makes pcvmenu acts like a script part for PassCore.
+So, the main modules won't ignore the functions and defined modules inside pcvmenu for vault-menu-items
+:) 
+"""
